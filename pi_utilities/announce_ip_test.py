029652f897e4e1aa01c3372fb4c6d2fafc0c332b
Max Fowler <maxhfowler@gmail.com>
2016-04-03 23:48:41 -0400

    just 1 test

--- pi_utilities/announce_ip_test.py
+++
@@ -1,6 +0,0 @@
-def announce_ip():
-    print '++ attempting test what'
-    raise Exception('blah')
-
-if __name__ == '__main__':
-    announce_ip()
--- pi_utilities/announce_ip_test2.py
+++
@@ -1,5 +0,0 @@
-def announce_ip():
-    print '++ attempting test what2'
-
-if __name__ == '__main__':
-    announce_ip()
