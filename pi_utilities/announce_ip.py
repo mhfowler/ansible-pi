d5ef55d5fb1296bff1d8c105cfad779cfc0532f0
Max Fowler <maxhfowler@gmail.com>
2016-04-03 23:48:19 -0400

    try 200 times

--- pi_utilities/announce_ip.py
+++ pi_utilities/announce_ip.py
@@ -1,11 +1,16 @@
 import socket
+import time
 
 from pi_utilities.slack_helper import slack_notify_message
 
 
 def announce_ip():
-    print '++ attempting to announce ip'
-    slack_notify_message('its pi')
+    index = 0
+    while index < 200:
+        print '++ attempting to announce ip: {}'.format(index)
+        slack_notify_message('its pi')
+        index += 1
+        time.sleep(1)
     # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # s.connect(("gmail.com",80))
     # ip_address = s.getsockname()[0]
